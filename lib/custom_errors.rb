95f1f7eee51e98c0fd7851a49a59b25fbe93bc0b
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:14:17 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -4,13 +4,16 @@ class Person
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
  
-  # Add the following two lines:
   class PartnerError < StandardError
   end
 end
