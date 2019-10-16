4ecdae3e0022fdf66e08082452fc3dde264effc7
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:15:58 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,6 +10,8 @@ class Person
     if person.class != Person
       begin
       raise PartnerError
+      rescue PartnerError => error
+          puts error.message
     else
       person.partner = self
     end
