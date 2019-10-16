8b7217529d75691c02220a4fbc83061961060216
Jared S <jaredjames2020@gmail.com>
2019-10-16 17:13:46 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,22 +7,14 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
     person.partner = self
   end
-  
-  class PartnerError < 
-    StandardError
+ 
+  # Add the following two lines:
+  class PartnerError < StandardError
   end
-
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
